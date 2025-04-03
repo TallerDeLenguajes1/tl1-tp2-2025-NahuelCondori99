f4f1d08f3ac2873af8533c66c37750a65ac0c0c0
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-04-03 02:00:27 -0300

    Agregue el procedimiento para mostrar la PC mas vieja

--- tp2_4.c
+++ tp2_4.c
@@ -12,6 +12,7 @@ struct compu
 }typedef computadora;
 
 void listarPCs(struct compu pc[], int cantidad);
+void mostrarMasVieja(struct compu pc[], int cantidad);
 
 int main()
 {
@@ -30,6 +31,7 @@ int main()
     }
 
     listarPCs(PC, 5);
+    mostrarMasVieja(PC, 5);
 
     return 0;
 }
@@ -48,6 +50,31 @@ void listarPCs(struct compu pc[], int cantidad)
 
         printf("\n");
     }
+    
+}
+
+void mostrarMasVieja(struct compu pc[], int cantidad)
+{
+    int indice, actual=2024;
+
+    for (int i = 0; i < cantidad; i++)
+    {
+        if (pc[i].anio < actual)
+        {
+            actual= pc[i].anio;
+            indice = i;
+        }
+        
+    }
+    printf("\n");
+
+    printf("PC mas vieja.\n");
+    printf("Anio de fabricacion: %d\n", pc[indice].anio);
+    printf("-Velocidad: %d\n", pc[indice].velocidad);
+    printf("-Cantidad de nucleos: %d\n", pc[indice].cantidad_nucleos);
+    printf("Tipo de procesador: %s", pc[indice].tipo_cpu);
+
+    printf("\n");
     
 }
 
